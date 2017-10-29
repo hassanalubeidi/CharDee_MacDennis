316a25c764305966d51e3d19e6f9b12f86653f97
hassanalubeidi <hassanalubeidi1996@gmail.com>
2017-10-29 19:32:10 +0000

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Player.create!({name: "Charlie", image_url: "charlie.jpg", team_name: "Thundercats"})
 Player.create!({name: "Mac", image_url: "mac.jpg", team_name: "Thundercats"})
 Player.create!({name: "Da Maniac", image_url: "da_maniac.jpg", team_name: "Thundercats"})
@@ -23,6 +16,83 @@ Player.create!({name: "Ryan McPoyle", image_url: "ryan_mcpoyle.png", team_name: 
 Player.create!({name: "Bill Ponderoza", image_url: "bill_ponderoza.jpg", team_name: "Franks Master Race"})
 Player.create!({name: "Charlie's mom", image_url: "charlies_mom.jpg", team_name: "Franks Master Race"})
 
+
 Team.create!({id: 3, name: "Thundercats", image_url: "thundercats.png", created_at: "2017-10-28 20:29:18", updated_at: "2017-10-28 21:23:18", game_id: nil})
+
 Team.create!({id: 5, name: "Golden Geese", image_url: "goldengeese.jpg", created_at: "2017-10-28 20:30:41", updated_at: "2017-10-28 20:30:41", game_id: nil})
-Team.create!({id: 6, name: "Franks Master Race", image_url: "franksteam.jpg", created_at: "2017-10-28 20:31:10", updated_at: "2017-10-28 20:31:10", game_id: nil})
+ Team.create!({id: 6, name: "Franks Master Race", image_url: "franksteam.jpg", created_at: "2017-10-28 20:31:10", updated_at: "2017-10-28 20:31:10", game_id: nil})
+
+Card.create!({back: "Chumbawamba",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "What is the greatest band in the world?"})
+Card.create!({back: "Becauze Denis is a bastardt man",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "Dennis is Asshole, Y Charlie hate?"})
+Card.create!({back: "Drink beer, huff glue, and eat cat food.",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "What do Charlie and Frank do to sleep through the meowing alley cats living outside their window?"})
+Card.create!({back: "Varies",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "What was the answer to the last two question?  First team to answer wins. (This card may not be won if there Card.create!({card_type: "have n"ot been two questions yet.)"})
+Card.create!({back: "Depends on whos in the room",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "Spell the last names of everyone in the room.  No mistakes may be made."})
+Card.create!({back: "Smashing people in the face with your big, red, sweaty balls",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "What is the best part of dodgeball?"})
+Card.create!({back: "How long",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "How long is the answer to this card."})
+Card.create!({back: "Cole (if a player answers with “Rory,” they get -1 card and have to finish their drink. ",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "Who can eat the most Olive Garden?"})
+Card.create!({back: "42 ",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "What is the answer to life, the Universe, and everything?"})
+Card.create!({back: "When the Event Horizon, a spacecraft that vanished years earlier, suddenly reappears, a team is dispatched to investigate the ship. Accompanied by the Event Horizon's creator, William Weir (Sam Neill), the crew of the Lewis and Clark, led by Capt. Miller (Laurence Fishburne), begins to explore the seemingly abandoned vessel. However, it soon becomes evident that something sinister resides in its corridors, and that the horrors that befell the Event Horizon's previous journey are still present. ",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "What is the plot of “Event Horizon”"})
+Card.create!({back: "Hummingbird, as they are legal tender",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " According to US Bird Law, what is a card_type of bird that cannot be kept as a pet?"})
+Card.create!({back: "Consult Cole",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " How would drunk (Person to your right) describe you in three words?"})
+Card.create!({back: "Oedipus",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " Who was the first mother fucker?"})
+Card.create!({back: "Milk steak, magnets, (funny little green) ghouls, and people’s knees",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " Name one of Charlie's favorite food, favorite hobby, and likes and dislikes?"})
+Card.create!({back: "Bruce Mathis; “that guy from 7th Heaven who got caught diddling kids” is also acceptable",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " Who is the biological father of Dennis and Dee?"})
+Card.create!({back: "Father Matthew Mara; “Street Rat” is also acceptable ",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " What is Rickety Cricket's real name?"})
+Card.create!({back: "Charlie Kelly – Charlie Day 
+Ronald McDonald – Rob McElhenney
+Dennis Reynolds – Glenn Howerton 
+Deandra Reynolds – Kaitlin Olson
+Frank Reynolds – Danny Devito
+",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " What are the first and last names of each of the gang members and the actor/actress who plays them?"})
+Card.create!({back: "D.E.N.N.I.S. system, the acronym:
+Demonstrate value 
+Engage physically 
+Nurture dependence 
+Neglect emotionally 
+Inspire hope 
+Separate entirely 
+The M.A.C. system, the acronym:
+Move in 
+After 
+Completion 
+Frank’s system was to gather the scraps by flashing Magnum condoms and a wad of $100’s.
+",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: " What are each of the steps of the D.E.N.N.I.S. system? If you can also identify the M.A.C. system and Frank's system, the other team must finish their drink."})
+Card.create!({color: "yellow", wildcard: false, level: "one", back: "Carmen, “The Tranny”",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "Who is the biological father of Dee's baby?"})
+Card.create!({color: "yellow", wildcard: false, level: "one", back: "Reggie Leach",wildcard: false, color: "yellow", level: "one", card_type: "Trivia", front: "What Philadelphia Flyer holds the franchise record for goals in a season?"})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "Wheelbarrow Drawing", front: "All Play - Both teams must elect an artist and a holder, and the person who drew this card selects a Topics card. The holder holds the artist's two feet off the ground only using his or her hands. The artist is given a marker and, using only their mouth to hold the marker, must draw the clue. First team to guess correctly in under 5 minutes wins, otherwise both teams finish their drinks.  If the artist's feet hit the floor, that team loses and must finish their drink."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "Sensual Footing", front: "All Play - One member from each team removes their socks and has to draw a picture of their most recent sexual position with their feet. First team to guess within two guesses wins. If neither team wins, select a new member for each team and repeat as necessary."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "A-Maze-ing", front: "All Play - Each team has 2 minutes to create a maze for the opposing team to solve. Swap mazes and race to finish first. If a maze is not solvable, the team which drew it must finish their drinks."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "The Great White North", front: " You have 1 minute to name all 13 Canadian Provinces. Teammates can help, but only one answer may be given at a time. Alternatively, correctly name all 13 Provinces and their capitals in under 5 minutes. If you fail to do so, whoever drew this card must recite the Pledge of Allegiance while your teammates drink so you can work on your patriotism. Hand over your heart, terrorist. (Appendix C lists the states and their capitals and the Canadian Pledge of Allegiance.)"})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "Charades", front: "Select one of the Topics cards and act out whatever it is. Your team has 1 minute to guess what you are acting out. One of the guessers may buy another minute (once) by drinking a full unit of their level’s beverage before the first minute is up."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "You Scratch My Back", front: "All Play - The artist(s) from your team must draw the clue from the Topics card on a teammate's back. The teammate must guess the clue solely from feel. If nobody gets it in 5 minutes, both teams lose and everyone must finish their drink."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "Spelling Bee", front: "Spell the word of the day from dictionary.com. If the opposing team gets a chance to steal, the original team attempting this card may pick any of the words in the drop down list of previous words of the day."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "Word Search", front: "Go to wordgames.com/daily-word-search.html and select 'Medium.' You have 1 minute to find four words in the word search. If the opposing team gets a chance to steal, they must select 'Easy' and find eight words in 1 minute."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "Mental Math", front: "The opposing team writes down two double-digit numbers. You have 2 minutes to multiply those numbers in your head and come up with the correct answer while the other team is allowed to shout numbers at you. An incorrect answer before the time is up results in losing the card."})
+Card.create!({color: "yellow", wildcard: false, level: "one", card_type: "Artistry", front: "All Play - See who can stack any objects in the room the highest in thirty seconds. The team with the tallest tower or the last team to knock over its tower wins. No interference."})
+
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Mac Day", front: "On the 6th day God created Man, and men are beef-cakes. Beef-cakes need to be greased up. The opposing team selects the beefiest of cakes from either team whom you must grease up from ankles to neck (excluding genitals...unless you're both into that). "})
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Purple Nurple", front: "You know what it is. Select one player from the opposing team to give you a 5 second purple nurple. If you show any outwards signs of pain, you do not get the card."})
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Drop and Give Me More", front: "All Play - Use Process of Elimination Selection to determine your opponent from the opposing team to compete in a push up contest. Judge sets the pace, first to drop loses. Winner keeps the card."})
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Dumbo", front: "Two players from the opposing team may flick the backs of your ears for 1 minute. Any outward signs of pain during this time results in losing this card. "})
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Tickle-Me-Elmo", front: "Any and all players from the opposing team have 30 seconds total to tickle you. You must not react to the tickling in any way. You and your team may not prevent them from tickling you."})
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Relay Race", front: "All Play - Using a predetermined area (e.g., around the house) each player from both teams must chug a beer and run a single lap. Before the next racer may begin running, he/she must then chug his/her beer before starting the next lap and so on so forth until every player has run."})
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Hot Shit!", front: "Eat two hot peppers and do not show any signs of pain for 1 minute after the peppers have been fully consumed. No drinking is allowed until the minute is over, and drinking anything other than the drink of your current round before your round is over will result in forfeiting this card even after you have won it. "})
+Card.create!({color: "black", wildcard: false, level: "two", wildcard: false,card_type: "Cotton Mouth", front: "Eat six Saltines in 1 minute. "})
+
+Card.create!({color:"black", wildcard: false, level: "three", wildcard: false, card_type: "Stage Fright", front: "You have 3 minutes (from the time you finish this card) to go to the bathroom and pee while the opposing team berates you through the door. If you manage to take a shit instead, you win the card AND everyone on the opposing team must finish their drinks."})
+Card.create!({color:"black", wildcard: false, level: "three", wildcard: false, card_type: "It’s Not Cheating if it’s Your Dog", front: "Give yourself a peanut butter goatee and let a dog or cat lick it off of your face. If no animals are available, the opposing team may choose a player from your team to lick it off your face."}) 
+Card.create!({color:"black", wildcard: false, level: "three", wildcard: false, card_type: "Come Over and Watch “Gilmore Girls”", front: "The opposing team has 5 minutes from the time they log into Kijiji to create an ad with your personal information (but they can pose as somebody else). You must provide the opposing team with at least your phone number if they do not have it. The ad cannot be removed until the end of the game."})
+Card.create!({color:"black", wildcard: false, level: "three", wildcard: false, card_type: "Drunk Dial", front: "Call a parent or guardian (grandparents, aunts, uncles, etc. are also acceptable). Speak to them for 1 minute as if you are completely intoxicated, which hopefully should not take much acting at this point in the game. If they do not answer, leave a 1 minute voicemail. If you cannot leave a voicemail, attempt with a different relative."})
+Card.create!({color:"black", wildcard: false, level: "three", wildcard: false, card_type: "Emotional Beratement", front: "The opposing team has the opportunity to say ANYTHING they want to insult you. Nothing is too far. Everything is in play. Player who selects this card must stand up and resist crying, smiling, or otherwise reacting for 2 minutes."})
+Card.create!({color:"black", wildcard: false, level: "three", wildcard: false, card_type: "Coming Clean", front: "Allow the opposing team to share three total secrets about you. Any personal information is fair game, as is anything that was previously sworn to secrecy. Even pinky promises do not have to be honored here. If the other team gets a chance to steal, your team can choose which player on their team can accept the challenge."})
+
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "Shot Roulette", front: "All Play - Put a shot glass in front of yourself and each of your teammates. Fill all but one shot glass with water and the other with a shot. Mix up the shots so the opposing team doesn't know which is which. Each player takes their shot one at a time. If the opposing team correctly guesses who had the shot (the judge or one player from the opposing team must be told in advance to confirm) they win the card."})
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "Cold Shower", front: "Without removing any articles of clothing except for shoes, take a 1 minute cold shower. Any time you get out from under the water the clock resets. By the end of the shower, your entire body must be wet."})
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "O Canada", front: "Flip a coin: heads in English, tails in French. You must sing the Canadian National Anthem (lyrics may be used from Appendix D) while standing on a chair and the opposing team circles around and yells boos and slurs. The person singing must sing with the previously recorded voice and not fall more than one line behind. Failure results in taking a shot of maple syrup."})
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "Golden Shower", front: "If you want this card, the opposing team is allowed to pour their own open beverages on you. You may not change clothes until the game is over."})
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "Employee of the Month", front: "You must fill out an online job application for Wal-Mart. You must put in proper information. The other team gets to put in your previous work history. You must sign in through facebook.com/walmartcareers and share the link when you are done with the application."})
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "The Scarlet Capturer AKA Leave it to Beaver", front: "Select someone from the opposing team. Place their underwear on your head. Once you have done so, you get the card. However, if you take the underwear off before the round ends, you forfeit the card."})
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "Dick Head", front: "Allow the opposing team to draw a penis on your forehead with a marker. Detail is encouraged, but coloring it in completely is discouraged. Just because they're a dick head doesn't mean you need to be an asshole."})
+Card.create!({color: "black", level: "three", wildcard: false, card_type: "Facebook Fail", front: "All Play - The opposing team selects a player to take as many shots as they want. For each shot taken, their team may post a status to your Facebook. The opposing team wins this card if any individual status gets at least 10 comments in 10 minutes from non-game participants. No soliciting comments from others."})
+
+
